f6146b3031d73a1aea535990f116882454c480c1
knowerofnothing <idcollege19@gmail.com>
2026-06-04 00:22:22 +0530

    C Program to check if a Number Is Positive Or Negative

---
+++ TOP100_CODES/prob1.c
@@ -0,0 +1,12 @@
+// C Program to check if a Number Is Positive Or Negative
+#include <stdio.h>
+int main() {
+  int num = 23;
+  if (num > 0)
+    printf("The number is positive");
+  else if (num < 0)
+    printf("The number is negative");
+  else
+    printf("Zero");
+  return 0;
+}
