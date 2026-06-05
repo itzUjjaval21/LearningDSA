eeb35fb1b7100c5fd1f2865de325ddd4d1719877
knowerofnothing <idcollege19@gmail.com>
2026-06-05 22:59:23 +0530

    Find the Greatest of the Three Numbers in C

---
+++ TOP100_CODES/prob6.c
@@ -0,0 +1,18 @@
+// Find the Greatest of the Three Numbers in C
+#include <stdio.h>
+int main() {
+  int num1, num2, num3;
+  printf("Enter 3 numbers:");
+  scanf("%d%d%d", &num1, &num2, &num3);
+  if (num1 > num2 && num1 > num3) {
+    printf("%d is greater", num1);
+  } else if (num2 > num1 && num2 > num3) {
+
+    printf("%d is greater", num2);
+
+  } else {
+
+    printf("%d is greater", num3);
+  }
+  return 0;
+}
